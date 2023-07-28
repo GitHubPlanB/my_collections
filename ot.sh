fe3ebf9e60ee7e45fc31bcc3aefe6c3d33736791
zhang yunyi <51401539+GitHubPlanB@users.noreply.github.com>
2023-07-28 10:03:14 +0800

    Update ot.sh

--- ot.sh
+++ ot.sh
@@ -1,2 +1,1 @@
-gnome-terminal --tab --title="rtl"          -- csh -c "cd rtl; exec csh"
-gnome-terminal --tab --title="design"       -- csh -c "cd design; exec csh"
+gnome-terminal --window --maximize -- csh -c "source ott.sh"
